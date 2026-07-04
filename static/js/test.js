15806ff5c83dc0675b49f7af84e545478ce802e9
bvaujour <bvaujour42@gmail.com>
2026-07-04 20:35:04 +0200

    lol

--- static/js/test.js
+++ static/js/test.js
@@ -1,95 +1,61 @@
 document.addEventListener("DOMContentLoaded", function ()
 {
-	const calendarEl1 = document.getElementById("calendar-1");
-
-	const calendar1 = new FullCalendar.Calendar(calendarEl1,
+	function	createCalendar(containerID)
 	{
-		initialView: "dayGridWeek",
-		locale: "fr",
-		firstDay: 1,
-
-		editable: true,
-		droppable: true,
-		selectable: true,
-
-		fixedWeekCount: false,
-		eventClick: function (info)
+		const calendarEl = document.getElementById(containerID);
+		const calendar = new FullCalendar.Calendar(calendarEl,
 		{
-			const confirmation = confirm(`Supprimer "${info.event.title}" du planning ?`);
-
-			if (confirmation)
-				info.event.remove();
-		},
-
-		headerToolbar:
-		{
-			left: "prev,next today",
-			center: "title",
-			right: "dayGridMonth dayGridWeek"
-		}
-	});
-	const calendarEl2 = document.getElementById("calendar-2");
-
-	const calendar2 = new FullCalendar.Calendar(calendarEl2,
-	{
-		initialView: "dayGridWeek",
-		locale: "fr",
-		firstDay: 1,
-
-		editable: true,
-		droppable: true,
-		selectable: true,
-
-		fixedWeekCount: false,
-		eventClick: function (info)
-		{
-			const confirmation = confirm(`Supprimer "${info.event.title}" du planning ?`);
-
-			if (confirmation)
-				info.event.remove();
-		},
-
-		headerToolbar:
-		{
-			left: "prev,next today",
-			center: "title",
-			right: "dayGridMonth dayGridWeek"
-		}
-	});
-	const calendarEl3 = document.getElementById("calendar-3");
-
-	const calendar3 = new FullCalendar.Calendar(calendarEl3,
-	{
-		initialView: "dayGridWeek",
-		locale: "fr",
-		firstDay: 1,
-
-		editable: true,
-		droppable: true,
-		selectable: true,
-
-		fixedWeekCount: false,
-		eventClick: function (info)
-		{
-			const confirmation = confirm(`Supprimer "${info.event.title}" du planning ?`);
-
-			if (confirmation)
-				info.event.remove();
-		},
-
-		headerToolbar:
-		{
-			left: "prev,next today",
-			center: "title",
-			right: "dayGridMonth dayGridWeek"
-		}
-	});
+			initialView: "dayGridWeek",
+			locale: "fr",
+			firstDay: 1,
+			editable: true,
+			droppable: true,
+			selectable: true,
+			fixedWeekCount: false,
+			eventClick: function (info)
+			{
+				const confirmation = confirm(`Supprimer "${info.event.title}" du planning ?`);
+				if (confirmation)
+					info.event.remove();
+			},
+			headerToolbar:
+			{
+				left: "prev,next today",
+				center: "title",
+				right: "dayGridMonth dayGridWeek"
+			}
+		});
+		return (calendar);
+	}
+	const calendar1 = createCalendar("calendar-1");
+	const calendar2 = createCalendar("calendar-2");
+	const calendar3 = createCalendar("calendar-3");
     calendar1.render();
     calendar2.render();
     calendar3.render();
-	fetch("/api/animateurs/")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
+
+	const animList = document.getElementById("animateurs-list");
+
+	fetch("/api/animateurs/").then(response => response.json()).then(data =>
+	{
+		data.forEach((animateur) =>
+		{
+			const div = document.createElement("div");
+			div.textContent = animateur.prenom + " " + animateur.nom;
+			div.classList.add("animateur");
+			animList.appendChild(div);
+		});
+	});
+
+    new FullCalendar.Draggable(animList,
+	{
+        itemSelector: ".animateur",
+        eventData: function (eventEl)
+		{
+			return {
+				title: eventEl.textContent,
+				allDay: true
+			};
+		}
     });
 })
