3775008460ec5df87ae2a3a949dbd6d4cc2d0a7a
bvaujour <bvaujour42@gmail.com>
2026-07-05 20:17:18 +0200

    lol

--- static/js/planning.js
+++ static/js/planning.js
@@ -1,45 +1,65 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const calendarEl = document.getElementById("calendar");
-    const animateursList = document.getElementById("animateurs-list");
+document.addEventListener("DOMContentLoaded", function ()
+{
+	let	selectedAnimateur;
 
-    new FullCalendar.Draggable(animateursList,
+	function	createCalendar(containerID)
 	{
-        itemSelector: ".animateur-draggable",
-        eventData: function (eventEl) {
-    return {
-        title: eventEl.dataset.title,
-        allDay: true
-    };
-}
+		const calendarEl = document.getElementById(containerID);
+		const calendar = new FullCalendar.Calendar(calendarEl,
+		{
+			initialView: "dayGridWeek",
+			locale: "fr",
+			firstDay: 1,
+
+			editable: true,
+			droppable: true,
+			selectable: true,
+
+			expandRows: true,
+
+			headerToolbar: false,
+			footerToolbar: false,
+			dateClick: function(info)
+			{
+				console.log(info);
+
+				console.log(info.dateStr);
+			}
+		});
+		return (calendar);
+	}
+	const calendar1 = createCalendar("calendar-1");
+	const calendar2 = createCalendar("calendar-2");
+	const calendar3 = createCalendar("calendar-3");
+    calendar1.render();
+    calendar2.render();
+    calendar3.render();
+
+	const animList = document.getElementById("animateurs-list");
+
+	fetch("/api/animateurs/").then(response => response.json()).then(data =>
+	{
+		data.forEach((animateur) =>
+		{
+			const div = document.createElement("div");
+			div.textContent = `${animateur.prenom} .${animateur.nom[0]}`;
+			div.classList.add("animateur");
+			div.addEventListener("click", () => {console.log(animateur)
+
+			});
+			animList.appendChild(div);
+		});
+
+		new FullCalendar.Draggable(animList,
+		{
+			itemSelector: ".animateur",
+
+			eventData: function (eventEl) {
+				return {
+					title: eventEl.textContent,
+					allDay: true
+				};
+			}
+		});
     });
-
-	const calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: "dayGridMonth",
-    locale: "fr",
-    firstDay: 1,
-
-    editable: true,
-    droppable: true,
-    selectable: true,
-
-    height: 600,
-	aspectRatio: 3,
-    contentHeight: 100,
-    fixedWeekCount: false,
-    dayMaxEventRows: 3,
-	eventClick: function (info)
-	{
-		const confirmation = confirm(`Supprimer "${info.event.title}" du planning ?`);
-
-		if (confirmation)
-			info.event.remove();
-	},
-
-    headerToolbar: {
-        left: "prev,next today",
-        center: "title",
-        right: "dayGridMonth dayGridWeek"
-    }
-});
-    calendar.render();
-});
+})
